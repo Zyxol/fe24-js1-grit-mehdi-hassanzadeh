1578da23c89dd851da3a30e7afce2cce34f25d37
Mehdi <149390266+Zyxol@users.noreply.github.com>
2024-11-08 14:27:45 +0100

    added base files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// JavaScript Template
+
+document.addEventListener("DOMContentLoaded", () => {
+    const headerTitle = document.querySelector("header h1");
+    headerTitle.textContent = "Welcome to My Project";
+    
+    console.log("JavaScript is working!");
+});
