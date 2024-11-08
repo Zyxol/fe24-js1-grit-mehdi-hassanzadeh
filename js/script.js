fcac0263d357d31df2cec1aa14fb279fb00daed0
Mehdi <149390266+Zyxol@users.noreply.github.com>
2024-11-08 15:40:47 +0100

    bare minimum except error handling is done
    +can fetch weather data
    +can display weather data
    +icon for weather description is displayed
    
    TODO: handle errors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-// JavaScript Template
+const API_KEY = '3f2993cef45c97c55329906f376ab82e';
 
-document.addEventListener("DOMContentLoaded", () => {
-    const headerTitle = document.querySelector("header h1");
-    headerTitle.textContent = "Welcome to My Project";
-    
-    console.log("JavaScript is working!");
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const city = document.querySelector("#city").value;
+
+    const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    // Fetch weather data for the entered city
+    fetch(WEATHER_URL)
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data); //Data content
+
+            const weatherDescription = data.weather[0].description;
+            const temperature = data.main.temp;
+            const windSpeed = data.wind.speed;
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            
+            document.querySelector("#weatherInfo").innerHTML = 
+            `<p>Deweza is: ${weatherDescription}</p>
+             <p>Detemperature is: ${temperature}°C</p>
+             <p>Dewind speed is: ${data.wind.speed} m/s</p>
+             <img src="${iconUrl}" alt="${weatherDescription} icon">`    
+
+        })
+        .catch(error => console.error('Error:', error));
 });
