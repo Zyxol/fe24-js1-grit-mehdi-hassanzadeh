8db65c4c7f19e77d3f8a05024103f10473ced0ee
Mehdi <149390266+Zyxol@users.noreply.github.com>
2024-11-15 09:35:31 +0100

    main feature implemented
    user input returns weather info about city. if no city found or error, website informs this to user
    todo-
    extra features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const API_KEY = '3f2993cef45c97c55329906f376ab82e';
 
+function logWeatherData(data) {
+    console.log(data);
+}
+
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
@@ -9,10 +13,19 @@ document.querySelector("form").addEventListener("submit", function(event) {
 
     // Fetch weather data for the entered city
     fetch(WEATHER_URL)
-        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (!response.ok && response.status === 404) {
+                throw new Error('City not found');
+            }
+            else if(!response.ok) {
+                throw new Error('An error occurred while fetching the weather data.');
+            }
+            return response.json();
+        })
         .then(data => {
 
-            console.log(data); //Data content
+            logWeatherData(data);
 
             const weatherDescription = data.weather[0].description;
             const temperature = data.main.temp;
@@ -21,11 +34,19 @@ document.querySelector("form").addEventListener("submit", function(event) {
             const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
             
             document.querySelector("#weatherInfo").innerHTML = 
-            `<p>Deweza is: ${weatherDescription}</p>
-             <p>Detemperature is: ${temperature}°C</p>
-             <p>Dewind speed is: ${data.wind.speed} m/s</p>
+            `<p>Weather: ${weatherDescription}</p>
+             <p>Temperature: ${temperature}°C</p>
+             <p>Wind Speed: ${data.wind.speed} m/s</p>
              <img src="${iconUrl}" alt="${weatherDescription} icon">`    
 
         })
-        .catch(error => console.error('Error:', error));
+            .catch(error => {
+            console.error(error);
+            if(error.message === 'City not found') {
+                document.querySelector("#weatherInfo").innerHTML = '<p>City not found</p>';
+            }
+            else {
+                document.querySelector("#weatherInfo").innerHTML = '<p>An error occurred while fetching the weather data.</p>';
+            }
+        })
 });
